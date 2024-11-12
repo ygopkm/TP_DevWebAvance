b61687b4b9c26d1a3ba083acc22af5e2f000bc9d
ygopkm <128469361+ygopkm@users.noreply.github.com>
2024-11-12 11:18:51 +0100

    Add files via upload

---
+++ NetBeansProjects/examen/src/main/java/com/exam/examen/ExamenApplication.java
@@ -0,0 +1,13 @@
+package com.exam.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+
+}
---
+++ NetBeansProjects/examen/src/test/java/com/exam/examen/ExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
